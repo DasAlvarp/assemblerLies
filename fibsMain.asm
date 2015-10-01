d3edbfb7ad564432ae72e9544cc76be7448c0d0a
dasalvarp <alvarogudis@gmail.com>
2015-10-01 11:44:37 -0500

    made it more efficient I think?

--- fibsMain.asm
+++ fibsMain.asm
@@ -2,14 +2,12 @@
 __start:
 	addi  	$s1, $s1, 0	#first fib number
 	addi	$s2, $s2, 0	#second fib number
-	addi	$s3, $s3, 0	#number of fibonaccis to be done. Probably not the technical term.
-	add	$s3, $s3, 8
+	add	$s3, $s3, 8	#number of fibonaccis to be done. Probably not the technical term.
 	add	$s2, $s2, 1	#set one fib number to 1
 	
-addLoop:add 	$s2, $s2, -1	#the fibbin' loop
-	add 	$t0, $s1, $s2	
-	add 	$s1, $s2, $zero
-	add 	$s2, $t0, $zero
-	bge	$s2, $zero, addLoop
-	addi  	$v0, $s2, 0
-	syscall
+addLoop:add 	$s3, $s3, -1	#the fibbin' loop
+	add 	$t0, $s1, $s2	#2 fib numbers being added to the next one.
+	add 	$s1, $s2, $zero	#first fib number being set to 2nd fib number
+	add 	$s2, $t0, $zero	#2nd fib number being set to the next fib number
+	bge	$s3, 0, addLoop	#if s3 (current max) >= 0 then loop, else, it's OVER
+	syscall			#failing to output 
