154248ac0bf0c436e12b4304e0ee83477541c6fc
dasalvarp <alvarogudis@gmail.com>
2015-10-21 11:22:20 -0500

    Finished asm1

--- no1.asm
+++ no1.asm
@@ -2,7 +2,7 @@
 #2.37
 .data
 	inputPrompt:	.asciiz "Please enter an integer:\n"
-	theString:	.space 32
+	theString:	.space 31
 .text
 main:
 #initialize variables
@@ -13,35 +13,36 @@ addi	$s2, $zero, 0	#giong to be the 2 in 8 + 2 = 10
 addi	$a2, $zero, 32
 
 #get input
-la	$a0, inputPrompt
-li	$v0, 54		#input
-la	$a1, theString	#load space into address
+
+li	$v0, 8		#input
+la	$a0, theString
+la	$a1, 32	#load space into address
 syscall
 
 
 stepTwo:
-	lb	$t1, 0($a1) 	# load the next character into t1
+	lb	$t1, 0($a0) 	# load the next character into t1
 	beqz	$t1, end 	# check for the null character
 #	bge 	$t1, 58, end1
 #	ble	$t1, 47, end
 	addi	$t1, $t1, -48
 	addi	$t2, $zero, 0	#x = 0
 	multiply:	#for multiplication. While x < t1 (its proper value) a+= 10, or 100 or whatever, x++
+		bge	$t2, $t1, unmultiply
 		add	$s0, $s0, $s1	#s0 
 		addi	$t2, $t2, 1	#x++
-		bge	$t2, $t1, unmultiply
 		j multiply
 	unmultiply:
 	
-	addi	$a1, $a1, 1 	# increment the string pointer
+	addi	$a0, $a0, 1 	# increment the string pointer
 	
 	sll	$s2, $s1, 1	#multiply s1 by 10
 	sll	$s1, $s1, 3
 	add	$s1, $s1, $s2
 j stepTwo # return to the top of the loop
 end:
-	addi	$a1, $s0, 0
-	li	$v0, 35
+	addi	$a0, $s0, 0
+	li	$v0, 1
 	syscall
 exit:
 
