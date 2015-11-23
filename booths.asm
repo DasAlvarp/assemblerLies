0dd21630e4a738b80f8f16966b984cf56f78b432
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2015-11-23 17:50:10 -0600

    added support for both variables
    ^^

--- booths.asm
+++ booths.asm
@@ -5,6 +5,12 @@
 	multiplier:	.space 32
 .text
 main:
+
+
+#s4 = multiplicand
+#s5 = multiplier
+#conversion done by copying and pasting old mips project for both variables. Very technical :P
+#loading multiplicand
 add	$v0, $zero, 54
 la	$a1, multiplicand
 la	$a0, multiplicandPrompt
@@ -12,3 +18,87 @@ addi	$a2, $zero, 32
 syscall
 la	$a0, multiplicand
 la	$t2, 0
+
+#initialize variables
+addi	$s0, $zero, 0 	#  string to binary thingy with the final number
+addi	$t0, $zero, 0	#  Going to hold value to add to s0 after calculations.
+addi	$s1, $zero, 1	# going to be in charge of multiplying by powers of 10, using sneaky maths. 
+addi	$s2, $zero, 0	# giong to be the 2 in 8 + 2 = 10
+addi	$s3, $zero, 0 	# is the number negative?
+
+#get input
+
+lb	$t1, 0($a0)
+beq	$t1, 45, neggy
+neggy: 
+	addi	$s3, $zero, 1
+	addi	$a0, $a0, 1
+	j stepTwo
+
+stepTwo:
+	lb	$t1, 0($a0) 	# load the next character into t1
+	beqz	$t1, end 	# check for the null character
+	beq	$t1, 10, end	# if it's equal to 10, or new line.
+	bgt 	$t1, 57, error	# is it ascii?
+	blt	$t1, 48, error
+	addi	$t1, $t1, -48
+	sll 	$s1, $s0, 3
+	sll 	$s0, $s0, 1
+	add	$s0, $s1, $s0	#total * 10
+	add	$s0, $s0, $t1 #total += latest digit
+	addi	$a0, $a0, 1 	# increment the string pointer
+
+j stepTwo # return to the top of the loop
+negative:
+	sub	$s4, $zero, $s0
+	j exit1
+end:
+	beq	$s3, 1, negative
+	add	$s4, $s0, 0
+	j exit1
+exit1:
+
+
+#multiplicand
+#initialize variables
+addi	$s0, $zero, 0 	#  string to binary thingy with the final number
+addi	$t0, $zero, 0	#  Going to hold value to add to s0 after calculations.
+addi	$s1, $zero, 1	# going to be in charge of multiplying by powers of 10, using sneaky maths. 
+addi	$s2, $zero, 0	# giong to be the 2 in 8 + 2 = 10
+addi	$s3, $zero, 0 	# is the number negative?
+
+#get input
+
+lb	$t1, 0($a0)
+beq	$t1, 45, neggy2
+neggy2: 
+	addi	$s3, $zero, 1
+	addi	$a0, $a0, 1
+	j stepTwo2
+
+stepTwo2:
+	lb	$t1, 0($a0) 	# load the next character into t1
+	beqz	$t1, end2 	# check for the null character
+	beq	$t1, 10, end2	# if it's equal to 10, or new line.
+	bgt 	$t1, 57, error	# is it ascii?
+	blt	$t1, 48, error
+	addi	$t1, $t1, -48
+	sll 	$s1, $s0, 3
+	sll 	$s0, $s0, 1
+	add	$s0, $s1, $s0	#total * 10
+	add	$s0, $s0, $t1 #total += latest digit
+	addi	$a0, $a0, 1 	# increment the string pointer
+
+j stepTwo # return to the top of the loop
+negative2:
+	sub	$s5, $zero, $s0
+	j exit
+end2:
+	beq	$s3, 1, negative2
+	add	$s5, $s0, 0
+	j exit
+
+exit:
+
+error:
+	li	$v0, -1
